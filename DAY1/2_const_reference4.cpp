ea652068e9348b62ce7439ba917b4388fe9f3612
ecourse <smkang@codenuri.co.kr>
2022-07-19 11:11:59 +0900

    Add files via upload

--- DAY1/2_const_reference4.cpp
+++ DAY1/2_const_reference4.cpp
@@ -11,4 +11,13 @@ int main()
 
 	std::cout << s1 << std::endl; // "abcdefghijklmnopqrstu"
 	std::cout << s2 << std::endl; // ""
+
+
+	const std::string s5 = "abcdefghijklmnopqrstu";
+	
+	std::string s6 = std::move(s5); // 핵심!!
+					// => 이코드는 move 생성자가 아닌 복사 생성자를 호출합니다
+					// => 즉, 자원의 이동이 아닌 자원의 복사 입니다.
+
+	// 핵심 : 상수객체는 move 될수 없습니다.
 }
