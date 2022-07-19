bf921c8b784247afe4b783ae06a198c71b90e122
ecourse <smkang@codenuri.co.kr>
2022-07-19 15:37:21 +0900

    Add files via upload

--- DAY1/3_const_member_function5-4.cpp
+++ DAY1/3_const_member_function5-4.cpp
@@ -4,19 +4,24 @@
 template<typename T>
 class RefCount
 {
-	int refcnt = 0;
+	mutable int refcnt = 0;
 public:
 	~RefCount() { std::cout << "~RefCount" << std::endl; }
 
-	void AddRef() { ++refcnt; }
+	// 참조계수 관련 멤버 함수는 일반적으로 "상수 멤버 함수"로 하게 됩니다.
+	// => 상수 객체도 수명관리 가능해야 합니다.
+	void AddRef() const { ++refcnt; }
 
-
-	void Release()			
+	void Release() const		
 	{
 		if (--refcnt == 0)
 			delete static_cast<T*>(this); 
 	}
 };
+
+
+
+
 
 class Truck : public RefCount< Truck  >
 {
@@ -28,8 +33,12 @@ public:
 int main()
 {
 	// 아래 코드를 생각해 봅시다.
+	// => p를 통해서는 객체를 "Read" 만 하겠다는 의도.
+	// => 읽기만 한다면 아래처럼 "const" 를 붙이는 것은 아주 좋은 코딩 스타일입니다.
 	const Truck* p = new Truck;
 
+	// 상수 객체라도 수명관리가 되어야 합니다.
+	// => 아래 2개의 함수는 반드시 "상수 멤버 함수" 로 해야 합니다.
 	p->AddRef();
 	p->Release();
 }
