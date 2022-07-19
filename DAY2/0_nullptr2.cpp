8a7889267c9b12066e103753cbe14e326c3fa304
ecourse <smkang@codenuri.co.kr>
2022-07-20 08:38:29 +0900

    Add files via upload

--- DAY2/0_nullptr2.cpp
+++ DAY2/0_nullptr2.cpp
@@ -1,7 +1,8 @@
 // caller : 함수와 인자를 받아서, 함수를 호출하는 도구
 template<typename F, typename ARG>
-void caller(F f, ARG arg) // int age = 0 인데.. f(arg) 하므로 error
-{
+void caller(F f, ARG arg) // int arg = 0 인데.. f(arg) 하므로 error
+{						  // std::nullptr_t arg = nullptr; 이므로
+						  // f(arg) 는 ok..
 	// 현재 시간 기록
 	f(arg);
 	// 함수 수행시간 출력
@@ -22,3 +23,12 @@ int main()
 	caller(foo, nullptr); // ok..  이렇게 사용하려고 nullptr을 만든것. 
 	
 }
+
+// 핵심
+// main						caller						foo(int* p)
+// foo(0)===============================================> ok..
+// caller(foo, 0) =======> int arg = 0 로 받음
+//						   foo(arg)======================> error
+
+// caller(foo, nullptr)==> std::nullptr_t arg = nullptr
+//							foo(arg) ====================> ok					
