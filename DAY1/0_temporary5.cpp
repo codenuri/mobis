57bb0bfd6e04c47927a57c035899d81b9f43f837
ecourse <smkang@codenuri.co.kr>
2022-07-19 10:10:44 +0900

    Add files via upload

---
+++ DAY1/0_temporary5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+struct Base
+{
+	int value = 10; 
+};
+struct Derived : public Base
+{
+	int value = 20;
+};
+int main()
+{
+	Derived d;
+	std::cout << d.value << std::endl; // 20
+
+	// 기반 클래스의 value 에 접근 하려면 아래처럼 캐스팅하면 됩니다.
+	// => 그런데, 아래 2줄의 차이점을 생각해 보세요.. 
+	std::cout << static_cast<Base&>(d).value << std::endl; // 10
+	std::cout << static_cast<Base>(d).value << std::endl; // 10
+}
+
+
+
+
+
+
+
