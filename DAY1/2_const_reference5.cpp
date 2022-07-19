ad9fe48fd9ec3c4beafb7d38e56ea89ab74bec95
ecourse <smkang@codenuri.co.kr>
2022-07-19 11:19:24 +0900

    Add files via upload

--- DAY1/2_const_reference5.cpp
+++ DAY1/2_const_reference5.cpp
@@ -10,7 +10,10 @@ public:
 	// 그런데, C++11 이후에는 많이 부족한 코드 입니다.
 	void setName(const std::string& s)
 	{
-		name = s;
+		name = s;             // 이 코드는 항상 복사 입니다.
+//		name = std::move(s);  // 이 코드는 항상 복사 입니다.
+							  // s가 const 이므로!!!
+							  // const 는 move 될수 없습니다.
 	}
 };
 
@@ -23,7 +26,7 @@ int main()
 	p.setName( s1);				// s1의 자원을 복사해라
 	p.setName( std::move(s2) );	// s2의 자원을 이동해라(뺏아가라는것)
 
-	std::cout << s1 << std::endl;
-	std::cout << s2 << std::endl;
+	std::cout << s1 << std::endl; // "john" 나와야 합니다.
+	std::cout << s2 << std::endl; // "" 나와야 합니다.
 }
 
