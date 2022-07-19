179a1139f8b4f6785ec9d6cdd14c75a6312c514e
ecourse <smkang@codenuri.co.kr>
2022-07-19 09:35:45 +0900

    Add files via upload

---
+++ DAY1/0_temporary4.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+class Point
+{
+public:
+	int x, y;
+
+	Point(int a, int b) { std::cout << "Point()" << std::endl; }
+	~Point() { std::cout << "~Point()" << std::endl; }
+};
+/*
+void f1(Point  pt) {} // call by value     : 복사본 생성
+void f2(Point& pt) {} // call by reference : 복사본 생성 안됨.
+
+int main()
+{
+	Point p(0, 0);
+	f1(p);
+	f2(p);
+}
+*/
+
+
+
+
+//-----------------------------------
+// 핵심 : "값 리턴" 과 "참조 리턴" 의 차이점을 완벽히 이해 해야 합니다. 
+Point pt(1, 2);
+
+Point f1() // return by value
+{
+	return pt;	// 이순간 pt가 반환되는것이 아니라
+				// pt를 복사한 "리턴용 temporary"가 반환 됩니다.
+}
+Point& f2() // return by reference
+{
+	return pt; // pt 자체를 반환 해달라는 의도
+				// temporary 가 생성되지 않습니다
+}
+int main()
+{
+	f1().x = 10; // error. 리턴용temporary.x = 10 이 됩니다.
+	f2().x = 10; // ok..   pt.x = 10 이 됩니다.
+}
+
+
+
