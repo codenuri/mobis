1f34947aa5b9adbd66f569d77e5e5002100a6656
ecourse <smkang@codenuri.co.kr>
2022-07-19 15:10:09 +0900

    Add files via upload

--- DAY1/3_const_member_function5-1.cpp
+++ DAY1/3_const_member_function5-1.cpp
@@ -15,14 +15,15 @@ public:
 			delete this;
 	}
 };
-
 int main()
 {
 	Car* p1 = new Car;
 	p1->AddRef(); // 규칙 1. 객체를 만들면 참조계수 증가
+
 	Car* p2 = p1;
 	p2->AddRef(); // 규칙 2. 객체주소를 복사해도 참조계수 증가
 
 	p1->Release();
-	p2->Release(); // 규칙 3. 객체 사용후 참조계수 감사
+	p2->Release(); // 규칙 3. 객체 사용후 참조계수 감소
+					// => 이순간 참조계수가 0이므로 delete 됩니다.
 }
