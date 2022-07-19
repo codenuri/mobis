7c0441eaaf72bf0056c2454f90f986bbe224d5fd
ecourse <smkang@codenuri.co.kr>
2022-07-19 10:18:02 +0900

    Add files via upload

--- DAY1/0_temporary5.cpp
+++ DAY1/0_temporary5.cpp
@@ -21,7 +21,7 @@ int main()
 
 	// 기반 클래스의 value 에 접근 하려면 아래처럼 캐스팅하면 됩니다.
 	// => 그런데, 아래 2줄의 차이점을 생각해 보세요.. 
-	std::cout << static_cast<Base&>(d).value << std::endl; // 10
+//	std::cout << static_cast<Base&>(d).value << std::endl; // 10
 	std::cout << static_cast<Base>(d).value << std::endl; // 10
 
 
@@ -29,7 +29,15 @@ int main()
 //	static_cast<Base&>(d).value = 30; // ok. 
 }
 
+// 결론 : 임시객체(temporary)는
 
+// 1. 개발자가 의도적으로 만들기도 하고
+//    => 함수 인자로 전달하는 경우
+//    => foo( Point(1,2))
+
+// 2. 개발자의 코드에 따라 생성되기도 합니다.
+//   => 값을 반환하는 함수
+//   => 값으로 캐스팅. 
 
 
 
