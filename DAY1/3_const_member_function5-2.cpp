139e118dc7aa78eb5b35d44d74eb6934d2c6009f
ecourse <smkang@codenuri.co.kr>
2022-07-19 15:20:03 +0900

    Add files via upload

--- DAY1/3_const_member_function5-2.cpp
+++ DAY1/3_const_member_function5-2.cpp
@@ -6,15 +6,24 @@ class RefCount
 {
 	int refcnt = 0;
 public:
-	~RefCount() { std::cout << "~RefCount" << std::endl; }
+	// 핵심 : "기반 클래스로 사용되는 클래스"는 반드시 소멸자가 가상함수 이어야 한다.
+	virtual ~RefCount() { std::cout << "~RefCount" << std::endl; }
 
 	void AddRef() { ++refcnt; }
-	void Release()
+
+	//	사용자 코드			// this의 원리,
+	void Release()			// void Release( RefCount* this )
 	{
 		if (--refcnt == 0)
-			delete this;
+			delete this;	// 여기서 this는 RefCount* 타입이라는 것이 핵심!
+							// 이때 소멸자가 제대로 호출되려면
+							// 소멸자를 가상함수로 해야 합니다.
 	}
 };
+// RefCount obj;
+// obj.Release() => Release(&obj)
+
+
 
 // Truck 도 참조계수 기술을 사용하고 싶다.
 class Truck : public RefCount
