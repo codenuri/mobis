3a34799aa04912252efef4efba4ef768c0e9f623
ecourse <smkang@codenuri.co.kr>
2022-07-20 14:17:07 +0900

    Add files via upload

--- DAY2/5_reference1-1.cpp
+++ DAY2/5_reference1-1.cpp
@@ -6,16 +6,18 @@ struct Point
 	int y;
 };
 
-void foo(Point& pt)       { std::cout << "Point&" << std::endl; }
-void foo(const Point& pt) { std::cout << "const Point&" << std::endl; }
-void foo(Point&& pt)      { std::cout << "Point&&" << std::endl; }
+void foo(Point& pt)       { std::cout<< "Point&" << std::endl; }	  //1
+void foo(const Point& pt) { std::cout<< "const Point&" << std::endl; }//2
+void foo(Point&& pt)      { std::cout<< "Point&&" << std::endl; }	  //3
 
 int main()
 {
 	Point pt;
 
-	foo(pt);
+	foo(pt);	// 1, 2 번이 모두 가능하지만, 우선순위가 정해져 있습니다.
+				// 1번 호출, 1번이 없으면 2번 호출
 
-	foo(Point());
+	foo(Point());// 2, 3 번이 모두 가능하지만, 우선순위가 정해져 있습니다.
+				 // 3번 호출, 3번이 없으면 2번 호출
 
 }
