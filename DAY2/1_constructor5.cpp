85ef9e250c5c5ab64ff8d5c302d0f3e1165e6efb
ecourse <smkang@codenuri.co.kr>
2022-07-20 10:23:07 +0900

    Add files via upload

--- DAY2/1_constructor5.cpp
+++ DAY2/1_constructor5.cpp
@@ -26,12 +26,15 @@ public:
 		std::cout << "Rect()" << std::endl;
 	}
 };
-
 class Rect2
 {
 	// 핵심 2. C++11 부터는 아래 처럼 해도 됩니다.
+	// => 그런데, 이코드는 결국 위와 완전히 동일합니다.
+	// => 필드 초기화에 있는 초기값을 가지고, 컴파일러가 생성자 초기화 리스트
+	//    로 옮겨 놓은것(위와 동일)
 	Point ptFrom{ 0, 0 };
 	Point ptTo{ 0, 0 };
+//	Point ptTo(0, 0); // error. 이 위치는 {}만 가능합니다.
 public:
 	Rect() 
 	{
