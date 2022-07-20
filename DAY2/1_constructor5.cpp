3da7c5ce66e9752950fae7217c63d77d5bb3fbf9
ecourse <smkang@codenuri.co.kr>
2022-07-20 10:18:23 +0900

    Add files via upload

--- DAY2/1_constructor5.cpp
+++ DAY2/1_constructor5.cpp
@@ -17,7 +17,11 @@ class Rect
 	Point ptTo;
 public:
 	// 사용자 코드	// 컴파일러가 변경한 코드
-	Rect()			// Rect() : ptFrom(), ptTo()  <= 핵심, 항상 디폴트 생성자 호출하는 코드 생성
+//	Rect()			// Rect() : ptFrom(), ptTo()  <= 핵심, 항상 디폴트 생성자 호출하는 코드 생성
+
+	// 핵심 1. 디폴트 생성자가 없는 타입이 멤버 데이타로 있는 경우
+	//		  사용자가 명시적으로 생성자를 호출하면 됩니다.
+	Rect() : ptFrom(0, 0), ptTo(0, 0)
 	{
 		std::cout << "Rect()" << std::endl;
 	}
