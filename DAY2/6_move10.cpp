fda6b92ff55afa58a39d74b225c6954839f9bb67
ecourse <smkang@codenuri.co.kr>
2022-07-20 16:40:22 +0900

    Add files via upload

---
+++ DAY2/6_move10.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+class Object
+{
+public:
+	Object() {}
+
+	Object(const Object&) 
+	{	
+		std::cout << "copy" << std::endl;	
+	}
+	// move 생성자를 만들때는
+	// 1. 되도록 예외가 발생하지 않게 만들고
+	// 2. 예외가 없다는 사실을 컴파일러에게 알려주세요
+	
+	// 함수() noexcept : 함수가 예외가 없음을 컴파일러에게 알리는 것
+
+	Object(Object&&) noexcept
+	{
+		std::cout << "move" << std::endl;
+	}
+};
+
+int main()
+{
+	Object o1;
+	Object o2 = o1; // 항상 복사 생성자
+	Object o3 = std::move(o1); // 항상 move 생성자
+
+	Object o4 = std::move_if_noexcept(o2); 
+					// move 생성자에 예외가 없다면(noexcept 가 붙었다면) move생성자
+					// noexcept 가 붙어 있지 않으면 복사 생성자
+
+
+
+	std::vector<Object> v(5);
+	std::cout << "----------------" << std::endl;
+
+	v.resize(10); // 이순간의 원리를 생각해봅시다.
+
+	std::cout << "----------------" << std::endl;
+}
+
