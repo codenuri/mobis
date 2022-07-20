25be08ab530e8a113d7c64ace444a27f9145dce2
ecourse <smkang@codenuri.co.kr>
2022-07-20 09:37:01 +0900

    Add files via upload

---
+++ DAY2/1_constructor3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+// 생성자 호출순서 를 알아 두세요.
+
+struct BM  // Base Member 라는 의미로 "BM" 이라고 했습니다.
+{
+	BM() { std::cout << "BM()" << std::endl; }
+	~BM() { std::cout << "~BM()" << std::endl; }
+};
+
+struct DM 
+{
+	DM() { std::cout << "DM()" << std::endl; }
+	~DM() { std::cout << "~DM()" << std::endl; }
+};
+struct Base
+{
+	BM bm;
+	
+	// 사용자코드		// 컴파일러가 변경한 코드
+	Base()			// Base() : bm()		=> call BM::BM()
+	{
+		std::cout << "Base()" << std::endl;
+	}
+	~Base() { std::cout << "~Base()" << std::endl; }
+};
+struct Derived : public Base
+{
+	DM dm;
+	
+	// 사용자코드		// 컴파일러가 변경한 코드
+	Derived()		// Derived() : Base(), dm() <= 순서가 중요합니다.
+	{											// 사용자가 변경할수 없습니다.
+		std::cout << "Derived()" << std::endl;
+	}
+	~Derived() { std::cout << "~Derived()" << std::endl; }
+};
+int main()
+{
+	Derived d; // 이 순간의 결과를 예측해 보세요.
+			   // call Derived::Derived() 입니다.
+}
