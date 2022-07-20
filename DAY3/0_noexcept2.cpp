067ae41a7d232e916d2700b2bba1f76ba6f3423c
ecourse <smkang@codenuri.co.kr>
2022-07-21 08:29:21 +0900

    Add files via upload

--- DAY3/0_noexcept2.cpp
+++ DAY3/0_noexcept2.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <string>
 
+void f1() {}
+
 class People
 {
 public:
 	std::string name;
 	int   age;
 public:
-	People(const std::string& s, int a)	: name(s), age(a), address(addr)
+	People(const std::string& s, int a)	: name(s), age(a)
 	{
 	}
 
@@ -15,12 +17,23 @@ public:
 	{
 		std::cout << "copy" << std::endl;
 	}
+	
+	// 핵심 1. move 생성자, move 대입 연산자를 만들때는 되도록 noexcept 붙이세요
+	// 2. 단, move 생성자 안에 있는 모든 코드가 예외 없음을 보장해야 합니다.
+	//  => STL 을 사용할때는 표준 문서를 참고 하면 됩니다.
+	//  => 사용자 정의 타입이나, 함수를 사용할때는 조사하면 됩니다.
 
-	People(People&& p) : name(std::move(p.name)), age(p.age) 
+	People(People&& p) 
+		noexcept( std::is_nothrow_move_constructible<std::string>::value
+			      && noexcept(f1()) )
+		: name( std::move(p.name) ), age(p.age) 
 	{
+		f1();
 		std::cout << "move" << std::endl;
 	} 
 };
+
+
 
 int main()
 {
