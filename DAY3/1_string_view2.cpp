9272d24292f4238a4a875274783894cf5dc453ab
ecourse <smkang@codenuri.co.kr>
2022-07-21 08:52:47 +0900

    Add files via upload

--- DAY3/1_string_view2.cpp
+++ DAY3/1_string_view2.cpp
@@ -2,6 +2,13 @@
 #include <string>
 #include <string_view>  
 
+void* operator new(std::size_t sz)
+{
+	printf("%d bytes alloc\n", sz);
+	return malloc(sz);
+}
+
+
 // 함수가 "읽기 전용 문자열"을 받는 다면
 // std::string 보다 std::string_view가 좋습니다.
 
@@ -17,6 +24,8 @@ int main()
 
 	// 아래 코드가 f1, f2 함수의 결정적인 차이점입니다.
 	// 잘 생각해 보세요
-	f1("abcdefghijklmnopqrstu");
+	// 아래 처럼 "abcdefghijklmnopqrstu" 라는 문자열 리터럴을 사용하면
+	// => "상수 메모리에 문자열이 놓이게 됩니다"
+//	f1("abcdefghijklmnopqrstu");
 	f2("abcdefghijklmnopqrstu");
 }
