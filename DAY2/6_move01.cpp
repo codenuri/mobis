02ee61845fc7538c7d112725d61162db4ec6578b
ecourse <smkang@codenuri.co.kr>
2022-07-20 14:32:59 +0900

    Add files via upload

---
+++ DAY2/6_move01.cpp
@@ -0,0 +1,17 @@
+﻿// move 개념 - 171 page~
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main()
+{
+	std::string s1 = "hello";
+	std::string s2 = "hello";
+	std::string s3 = s1;	// 자원의 복사	
+	std::string s4 = std::move(s2); // 자원의 이동
+
+	std::cout << s1 << std::endl; // "hello"
+	std::cout << s2 << std::endl; // ""
+
+}
+
