ac8299b97018792e498e6465c4cc0bd25faa706e
ecourse <smkang@codenuri.co.kr>
2022-07-20 10:16:41 +0900

    Add files via upload

---
+++ DAY2/1_constructor5.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+class Point
+{
+	int x, y;
+public:
+//	Point() : x(0), y(0) { std::cout << "Point()" << std::endl; }
+	Point(int a, int b) : x(a), y(b) { std::cout << "Point(int, int)" << std::endl; }
+};
+// 핵심 : Point 에 디폴트 생성자가 없다는 점
+// Point p1;		// error
+// Point p2(0,0);	// ok
+
+class Rect
+{
+	Point ptFrom;
+	Point ptTo;
+public:
+	// 사용자 코드	// 컴파일러가 변경한 코드
+	Rect()			// Rect() : ptFrom(), ptTo()  <= 핵심, 항상 디폴트 생성자 호출하는 코드 생성
+	{
+		std::cout << "Rect()" << std::endl;
+	}
+};
+
+int main()
+{
+	Rect r;
+}
+
+
+
+
