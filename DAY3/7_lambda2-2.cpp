ff7f99e2b8f05b28f970a32707f4224bc5122cb5
ecourse <smkang@codenuri.co.kr>
2022-07-21 15:29:59 +0900

    Add files via upload

--- DAY3/7_lambda2-2.cpp
+++ DAY3/7_lambda2-2.cpp
@@ -22,5 +22,16 @@ int main()
 
 	int k = 3;
 	IsMod f(k); // k를 캡쳐한 단항 항수
+
+	bool b = f(10); // 10 이 k의 배수인지 조사.. false
+
 	auto ret2 = std::find_if(v.begin(), v.end(), f);
+
+
+	auto ret3 = std::find_if(v.begin(), v.end(), f1); // 함수만 전달
+
+	auto ret4 = std::find_if(v.begin(), v.end(), IsMod(k) ); // 임시객체로전달
+										//=> k를 캡쳐한 단항 함수객체
+
+
 }
