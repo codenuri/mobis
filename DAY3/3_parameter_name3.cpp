58b07d715a318cc5484c729a02c3d56f51f607be
ecourse <smkang@codenuri.co.kr>
2022-07-21 10:04:16 +0900

    Add files via upload

--- DAY3/3_parameter_name3.cpp
+++ DAY3/3_parameter_name3.cpp
@@ -12,13 +12,23 @@ template<typename T> class lock_guard
 	T& mtx;
 public:
 	lock_guard(T& m) : mtx(m) { mtx.lock(); }
-	~lock_guard() { mtx.unlock(); }
+	~lock_guard()             { mtx.unlock(); }
 };
-
 
 void foo()
 {
-	lock_guard<std::mutex> g(m);
+//	lock_guard<std::mutex> g(m); // 생성자에서 m.lock() 수행
+
+	// mutex 를 획득(lock)하는 것은 여러가지 방법이 있습니다.
+//	m.lock();		// 획득하지 못하면 대기
+//	m.try_lock();	// 획득하지 못하면 대기하지 않고 false 반환
+
+	if (m.try_lock())
+	{
+		//....
+
+		m.unlock();
+	}
 
 }
 
