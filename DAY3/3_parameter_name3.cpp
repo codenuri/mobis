1f2bda6b3e51ec70f7528e51cd201409a41bf047
ecourse <smkang@codenuri.co.kr>
2022-07-21 10:01:35 +0900

    Add files via upload

---
+++ DAY3/3_parameter_name3.cpp
@@ -0,0 +1,30 @@
+// F9. 사용되지 않은 파라미터는 이름이 있어서는 안된다. ( 8page)
+
+#include <iostream>
+#include <mutex>
+
+std::mutex m;
+int shared_data = 0;
+
+
+template<typename T> class lock_guard
+{
+	T& mtx;
+public:
+	lock_guard(T& m) : mtx(m) { mtx.lock(); }
+	~lock_guard() { mtx.unlock(); }
+};
+
+
+void foo()
+{
+	lock_guard<std::mutex> g(m);
+
+}
+
+
+
+int main()
+{
+
+}
