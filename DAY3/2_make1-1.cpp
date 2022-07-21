6d5163e274c1d99aa06a7918360d67125229dea6
ecourse <smkang@codenuri.co.kr>
2022-07-21 09:25:52 +0900

    Add files via upload

--- DAY3/2_make1-1.cpp
+++ DAY3/2_make1-1.cpp
@@ -17,9 +17,14 @@ int main()
 {
 	std::pair<int, double> p(1, 3.4);
 
-	List<std::pair<int, double>>   s3(10, p); 
+	// 클래스 템플릿을 직접 사용하면 항상 코드가 복잡해 보입니다.
+	List<std::pair<int, double>> s1(10, p); 
 
-	auto s = make_list(10, p); // 위와 동일한 의미.
+	// 하지만, 클래스 템플릿의 객체를 생성하는 함수 템플릿을 사용하면
+	// 코드가 간결해 집니다.
+	// => "Object Generator" 라고 불리는 기술
+	auto s2 = make_list<std::pair<int, double>> (10, p); // 위와 동일한 의미.
+	auto s3 = make_list(10, p);
 }
 
 
