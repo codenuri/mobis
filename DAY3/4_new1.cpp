44b42860dd07e4e9da86926728f6c761bbb21d89
ecourse <smkang@codenuri.co.kr>
2022-07-21 10:53:50 +0900

    Add files via upload

---
+++ DAY3/4_new1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class Point
+{
+	int x, y;
+public:
+	Point(int a, int b) { std::cout << "Point()" << std::endl; }
+	~Point() { std::cout << "~Point()" << std::endl; }
+	Point(const Point&) { std::cout << "Point(const Point&)" << std::endl; }
+};
+int main()
+{
+	// new가 하는 일
+	// 1. 메모리 할당
+	// 2. 생성자 호출
+//	Point* p1 = new Point(1, 2);
+
+	// 1. 소멸자 호출
+	// 2. 메모리 해지 
+//	delete p1;
+
+	// 1. 메모리만 할당하기
+	Point* p2 = operator new(sizeof(Point));
+
+
+	// 4. 메모리만 해지
+	operator delete(p2);
+
+}
