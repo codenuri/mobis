3bc076bc5ea0d0354d1792eef45672bd96152aba
ecourse <smkang@codenuri.co.kr>
2022-07-21 15:09:29 +0900

    Add files via upload

---
+++ DAY3/6_완벽한전달9.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+class People
+{
+	std::string name;
+	std::string addr;
+public:
+	/*
+	// 아래 생성자는 C++98 이라면 완벽한 코드입니다
+	People(const std::string& name, const std::string& addr) : name(name), addr(addr)
+	{
+		std::cout << "ctor 1" << std::endl;
+	}
+
+	// 하지만 C++11의 move를 지원하려면 3개가 더 필요..
+	People(std::string&& name, const std::string& addr) : name(std::move(name)), addr(addr)
+	{
+		std::cout << "ctor 2" << std::endl;
+	}
+	People(const std::string& name, std::string&& addr) : name(name), addr(std::move(addr))
+	{
+		std::cout << "ctor 3" << std::endl;
+	}
+	People(std::string&& name, std::string&& addr) : name(std::move(name)), addr(std::move(addr))
+	{
+		std::cout << "ctor 4" << std::endl;
+	}
+	*/
+
+	// 위처럼 직접 만들면 너무 많은 함수를 만들어야 합니다.
+	// 이때 forwarding reference를 사용합니다.
+	template<typename T1, typename T2>
+	People(T1&& s1, T2&& s2)
+		: name(std::forward<T1>(s1)), addr(std::forward<T2>(s2))
+	{
+	}
+
+};
+
+int main()
+{
+	std::string s1 = "john";
+	std::string s2 = "seoul";
+
+	People p1(s1, s2); 
+	People p2(std::move(s1), s2); 
+	People p3(s1, std::move(s2)); 
+								  
+	People p4(std::move(s1), std::move(s2));
+
+}
