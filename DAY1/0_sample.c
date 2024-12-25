fd75b4ff11b5fcfecc7220d656f00a617c159af1
ecourse <smkang@codenuri.co.kr>
2024-12-26 08:40:41 +0900

    Add files via upload

---
+++ DAY1/0_sample.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("hello\n");
+}
+
+// vscode 에서 터미널 창 열기
+// => terminal 메뉴
+// => 또는 단축키, Ctrl + ` (ESC 아래 키보드)
+// => 기본은 powershell
+// => 첨부된 배치 파일 사용하려면
+//    Command prompt 로 변경해서 사용하세요
+
+
+// 빌드 하는 법 
+// => gcc 0_sample.c  => a.exe 생성
+
+
+// 빌드 + 실행을 한번에 (gccbuild.bat 참고)
+// => gccbuild 0_sample.c
+
+
+// 일부 C++ 을 사용하는 예제 있음
+// => 멀티스레드 관련예제에서 "스레드 생성"을 위해 C++ 사용
+// 빌드      : g++ 소스.cpp
+// 빌드+실행 : gppbuild 소스.cpp
+
