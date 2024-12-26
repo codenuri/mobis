a72028e971ef27330a05d4dc8bf98d6b1bd65160
ecourse <smkang@codenuri.co.kr>
2024-12-26 10:39:22 +0900

    Add files via upload

--- DAY1/2_file_format6.c
+++ DAY1/2_file_format6.c
@@ -14,9 +14,13 @@ int main(void)
 				// => 실행시간에 스택에는 포인터 변수(sp)만 생성되고
 				// => sp 가 .rdata 섹션에 있는 문자열을 가리키게 됩니다.
 
-    *sa = 'A'; // ok. 스택은 R/W 가능
-//    *sp = 'A'; // runtime error. sp 가 가리키는 곳은
+//    *sa = 'A'; // ok. 스택은 R/W 가능
+    *sp = 'A'; // runtime error. sp 가 가리키는 곳은
 				// 상수 메모리(.rdata) 이므로 
 
 	printf("finish\n");
 }
+// 정리
+// 문자열 배열(지역변수) : 스택에 문자열이 있으므로 R/W 가능
+// 문자열 포인터        : 문자열 자체는 ".rdata" 에 있다.
+//						 상수 메모리 이므로 Read 만 가능. Write 할수 없다. (runtime error)
