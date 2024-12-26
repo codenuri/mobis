eed1bc45abe8a0f62c428f862fa7a3a4ccfb8eaa
ecourse <smkang@codenuri.co.kr>
2024-12-26 16:20:36 +0900

    Add files via upload

---
+++ DAY1/6_vector3.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main()
+{
+	// 4. 사용자가 -1을 입력할때 까지 계속 입력 받고 싶습니다.
+	//    단, 입력된 모든 값은 보관되어 있어야 합니다.
+	int n = 0;
+
+	int score[4];
+	int count = 0;
+
+	while (1)
+	{
+		scanf("%d", &n);
+
+		if (n == -1)
+			break;
+
+		score[count++] = n;
+	}
+
+}
+
+
