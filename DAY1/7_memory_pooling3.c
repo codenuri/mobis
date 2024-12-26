e29d14003a8fb927060dcbc7fcb0b704388b3896
ecourse <smkang@codenuri.co.kr>
2024-12-26 15:03:10 +0900

    Add files via upload

--- DAY1/7_memory_pooling3.c
+++ DAY1/7_memory_pooling3.c
@@ -16,6 +16,28 @@ typedef struct _bullet
 } bullet;
 
 
+#define SIZE 200
+
+bullet* pool_head = 0;
+
+void init_pool()
+{
+	// #1. 200개 총알을 한번에 메모리 할당
+	pool_head = (bullet*)malloc(sizeof(bullet) * SIZE);
+
+	// #2. 모든 총알을 list 형태로 연결
+	for( int i = 0; i < SIZE -1; i++)
+	{
+		pool_head[i].next = &(pool_head[i+1]);
+	}
+}
+
+
+
+
+
+
+
 int main()
 {
 
