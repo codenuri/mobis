623df8694dfbc7d79039bb36c242cb56578ad3ea
ecourse <smkang@codenuri.co.kr>
2024-12-26 15:38:32 +0900

    Add files via upload

---
+++ DAY1/8_flexible_array1.c
@@ -0,0 +1,26 @@
+﻿// 114 page. flexible_array
+#include "chronometry.h"
+
+typedef struct _point
+{
+	int x;
+	int y;
+} POINT;
+
+// Point를 여러개 관리하는 구조체 
+// => 아래 구조체는 10개의 Point 를 저장하게 됩니다.
+// => 가변 갯수로 만들수 없을까요 ?
+typedef struct _shapes
+{
+	int count;		// 점의 갯수 
+	POINT pos[10];  
+} SHAPES;
+
+
+int main()
+{
+	SHAPES s;
+	s.count = 5;
+	s.pos[0].x = 10;
+	s.pos[1].x = 10;
+}
---
+++ DAY1/8_flexible_array2.c
@@ -0,0 +1,30 @@
+﻿// 114 page. flexible_array
+#include "chronometry.h"
+
+typedef struct _point
+{
+	int x;
+	int y;
+} POINT;
+
+// 해결책 1. 배열 대신 포인터 사용하면 어떨까 ?
+
+typedef struct _shapes
+{
+	int count;		
+	POINT* pos
+} SHAPES;
+
+int main()
+{
+	SHAPES s;
+	s.count = 5;
+	s.pos = malloc(sizeof(POINT) * s.count);
+
+	// s.pos 는 포인터 이지만 배열 처럼 가능하므로 아래 처럼 사용해도 됩니다.
+	s.pos[0].x = 10;
+	s.pos[0].y = 10;
+
+	s.pos[1].x = 10;
+	s.pos[2].y = 10;
+}
