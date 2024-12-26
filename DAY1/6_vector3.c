36991536218cc2494eecc6e367a09aba97dd2307
ecourse <smkang@codenuri.co.kr>
2024-12-26 16:23:21 +0900

    Add files via upload

--- DAY1/6_vector3.c
+++ DAY1/6_vector3.c
@@ -9,7 +9,13 @@ int main()
 	//    단, 입력된 모든 값은 보관되어 있어야 합니다.
 	int n = 0;
 
-	int score[4];
+//	int score[4];	// 이렇게 하면 필요 없을때 제거할수 없습니다.
+					// 지역변수의 수명이 개발자가 제어할수 없음.
+
+	int size = 4;
+
+	int* score = (int*)malloc(sizeof(int)*size);
+
 	int count = 0;
 
 	while (1)
