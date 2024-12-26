5bd2731415c4cd83f111b699eb228a757389d1b8
ecourse <smkang@codenuri.co.kr>
2024-12-26 14:19:09 +0900

    Add files via upload

---
+++ DAY1/5_memory_allocation4.cpp
@@ -0,0 +1,45 @@
+﻿#include "chronometry.h"
+#include <stdlib.h>
+#include <vector>
+#include <array>
+
+const int count = 10000000;
+
+void use_stack()
+{
+	int x[1000];
+	x[0] = 0;
+}
+
+void use_heap()
+{
+	std::vector<int> v(1000); // 동적 메모리 할당
+	v[0] = 0;
+}
+
+void ex1()
+{
+	for (int i = 0; i < count; i++)
+	{
+		use_stack();
+	}
+}
+
+void ex2()
+{
+	for (int i = 0; i < count; i++)
+	{
+		use_heap();
+	}
+}
+
+int main()
+{
+
+	CHRONOMETRY(ex1);
+	CHRONOMETRY(ex2);
+}
+
+
+
+
