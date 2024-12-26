e0dc3765a3a5fbfb78b5107441241759a031a71c
ecourse <smkang@codenuri.co.kr>
2024-12-26 09:14:35 +0900

    Add files via upload

---
+++ DAY1/1_chronometry_example3.c
@@ -0,0 +1,22 @@
+﻿#include "chronometry.h"
+
+const unsigned long long count = 10000000;
+
+void ex1(int a, int b)
+{
+	for (unsigned long long i = 0; i <= count; i++)
+	{
+		int n = a + b;
+	}
+}
+
+int main()
+{
+	// CHRONOMETRY 사용법
+	// => CHRONOMETRY(함수이름, arg1, arg2, arg3, .... )
+	// => 인자 갯수 제한이 없습니다.
+	
+	// 인자가 있는 함수도 사용가능.
+	CHRONOMETRY(ex1, 1, 2);
+
+}
