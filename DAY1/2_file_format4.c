54ffe1e8b8fd192485e1b00c59b20c6a1d4ff9cb
ecourse <smkang@codenuri.co.kr>
2024-12-26 10:23:00 +0900

    Add files via upload

--- DAY1/2_file_format4.c
+++ DAY1/2_file_format4.c
@@ -11,9 +11,17 @@ char g2[] = "abcdefg";
 
 void foo(void)
 {
+	// foo의 지역변수 만큼 스택 확보- 내일 자세히
+	// => lx, p 의 크기만큼 확보
+
     static int sx = 0xAABBCCDD;
+	// => 이미 초기값이 메모리에 있음.
+	// => 실행시 넣는 것이 아님
+	// => 컴파일시 .data 섹션에 초기값 있음.
 
     int lx = 0x55667788;
+	// => 이코드는 실행시 스택에 값을 넣은것
+	// => mov lx위치(스택주소), 0x55667788
     
 	printf("hello world\n");
 
