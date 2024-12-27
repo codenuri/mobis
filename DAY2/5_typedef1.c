3ce36c10317b69734eaf03fc7571db6c9bbcbf22
ecourse <smkang@codenuri.co.kr>
2024-12-27 09:31:40 +0900

    Add files via upload

---
+++ DAY2/5_typedef1.c
@@ -0,0 +1,12 @@
+﻿// typedef 해석 방법
+// typedef A B => B 는 A의 alias 이다. 라고 해석하면
+//									  복잡한 typedef 를 해석할수 없습니다
+
+typedef int DWORD;
+typedef int ARR[2];
+
+int main()
+{
+	DWORD n;
+	ARR   a;
+}
