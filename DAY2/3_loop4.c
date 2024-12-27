c2a180b1d5e462780aad5f929799cba337311357
ecourse <smkang@codenuri.co.kr>
2024-12-27 09:18:52 +0900

    Add files via upload

---
+++ DAY2/3_loop4.c
@@ -0,0 +1,34 @@
+﻿#include "chronometry.h"
+
+// 반복문에서 함수 호출 제거
+
+void bad()
+{
+	char s[] = "to be or not to be";
+
+	// 아래 코드는 strlen() 함수가 문자열의 길이 만큼 호출됩니다.
+	for (int i = 0; i < strlen(s); i++)
+	{
+		if (s[i] == ' ')
+			s[i] = '*';
+	}
+}
+
+void good()
+{
+	char s[] = "to be or not to be";
+
+	int sz = strlen(s);
+	
+	for (int i = 0; i < sz; i++)
+	{
+		if (s[i] == ' ')
+			s[i] = '*';
+	}
+}
+
+int main()
+{
+	bad();
+	good();
+}
