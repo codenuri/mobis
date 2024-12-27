f4f0812b06e6925f14da938d4f27ea2c5ea3ae82
ecourse <smkang@codenuri.co.kr>
2024-12-27 09:52:37 +0900

    Add files via upload

--- DAY2/5_typedef5.c
+++ DAY2/5_typedef5.c
@@ -1,16 +1,23 @@
 // 방법 #2. 구조체 포인터를 사용하는 기술 
 // => windows, linux, osx 등의 OS 가 내부적으로 사용하는 기술
 
-struct _HICON {};
-struct _HBITMAP {};
+// 아래 구조체 들은 "구조체" 자체를 사용 하라고 만든것이 아닙니다.
+// => 구조체의 포인터 타입만 사용하려고 만든것 입니다.
+// => 아무 멤버도 없는 구조체를 허용하지 않은 컴파일러도 있습니다.
+//    이럴때는 에러는 피하기 위해서, 아무 멤버나 하나 넣으면 됩니다.
+struct _HICON   { int not_used;};
+struct _HBITMAP { int not_used;};
 
-typedef _HICON*   HICON;
-typedef _HBITMAP* HBITMAP;
+typedef struct _HICON*   HICON;
+typedef struct _HBITMAP* HBITMAP;
 
+// 서로 다른 구조체의 포인터 타입은 다른 타입 이므로
+// => HICON, HBITMAP 은 모두 다른 타입입니다.
+// => 또한, 포인터(주소)도 결국 정수이므로, 포인터 타입을 "정수를 담는 용도로" 사용해도 됩니다.
 
 HICON CreateIcon() 
 {
-	 return 10; 
+	 return (HICON)10; 
 } 
 
 void DrawBitmap(HBITMAP id) 
@@ -21,5 +28,6 @@ int main()
 {
 	HICON icon_id = CreateIcon();
 
-	DrawBitmap(icon_id); 
+	DrawBitmap(icon_id); // 이제는 컴파일 에러!!	
+						 // 잘못 사용할수 없으므로, 안정성이 향상 되었습니다.
 }
